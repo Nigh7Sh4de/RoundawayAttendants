16a17614f7967982cbd8ee7f85066f3f6d0b08dc
nigh7sh4de <plotnikd@gmail.com>
2016-10-13 02:29:24 -0400

    Add login page

--- www/js/app.js
+++ www/js/app.js
@@ -3,22 +3,64 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    $ionicPlatform.ready(function() {
+        if(window.cordova && window.cordova.plugins.Keyboard) {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+            // Don't remove this line unless you know what you are doing. It stops the viewport
+            // from snapping when text inputs are focused. Ionic handles this internally for
+            // a much nicer keyboard experience.
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if(window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
 })
+.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    	.state('login', {
+            url: '/login',
+            templateUrl: 'templates/login.html',
+            controller: 'LoginController'
+        })
+    	.state('welcome', {
+            url: '/Welcome',
+            templateUrl: 'templates/Welcome.html',
+            controller: 'WelcomeController'
+        });
+
+    $urlRouterProvider.otherwise('/login');
+})
+
+app.controller("LoginController", function($scope, $stateParams, $state) {
+ 
+    $scope.login = function() {
+        console.log('log in!')
+        facebookConnectPlugin.getLoginStatus(function(success){
+            console.log('got status!', status);
+            if(success.status === 'connected'){
+                console.log('already logged in :D');
+                $state.go('welcome');
+            }
+            else {
+                console.log('logging in...');
+                facebookConnectPlugin.login(['email', 'public_profile'], function(response) {
+                    console.log('logged in!', response);
+                    $state.go('welcome');
+                }, console.error);
+            }
+        });
+    }
+ 
+});
+
+app.controller("WelcomeController", function($scope, $stateParams) {
+ 
+ 
+});
