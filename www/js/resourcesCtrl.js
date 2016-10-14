90b1792c198483932b349f5da338dcf79c0e882a
Dennis Plotnik <adknowledgeuser@android-74633fd597416a80.adknowledge.com>
2016-10-14 09:48:25 -0400

    Add resource service

--- www/js/resourcesCtrl.js
+++ www/js/resourcesCtrl.js
@@ -1,24 +1,12 @@
-angular.module('starter').controller("ResourceList", function($scope, $stateParams, $state) {
+angular.module('starter').controller("ResourceList", function($scope, $stateParams, $state, resourceService) {
 
-    $scope.spots = [{
-        id: '123456789012345678901234',
-        name: 'Spot #1',
-        location: {
-            address: '123 Fake St. Cityville, ON 1Z2X3C'
-        }
-    }, {
-        id: '123456789012345678901235',
-        name: 'Spot #2',
-        location: {
-            address: '456 Derp Ave. Cityville, ON 1Z2X3C'
-        }
-    }, {
-        id: '123456789012345678901236',
-        name: 'Spot #3',
-        location: {
-            address: '789 Good Ln. Cityville, ON 1Z2X3C'
-        }
-    }]
+    $scope.resources = resourceService;
+
+    $scope.context = 'lots';
+
+    $scope.setContext = function(newContext) {
+        $scope.context = newContext;
+    }
 
     $scope.viewResourceDetails = function(type, id) {
         $state.go('resourceDetails', {
