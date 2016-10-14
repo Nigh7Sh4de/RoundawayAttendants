e89a1b13bebaaf4942eb4b49721132ffac830f07
Dennis Plotnik <adknowledgeuser@android-74633fd597416a80.adknowledge.com>
2016-10-14 16:40:01 -0400

    Add tap listeners to add/remove availability

--- www/js/resourceDetailsCtrl.js
+++ www/js/resourceDetailsCtrl.js
@@ -1,13 +1,20 @@
-angular.module('starter').controller("ResourceDetails", function($scope, $stateParams, $state, resourceService) {
-    $scope.back = function() {
+angular.module('starter').controller("ResourceDetails", function ($scope, $stateParams, $state, resourceService) {
+    $scope.spot = resourceService[$stateParams.type].filter(s => s.id == $stateParams.id)[0];
+
+    $scope.back = function () {
         $state.go('resourceList')
     }
 
-    $scope.searchCarsInResource = function() {
+    $scope.searchCarsInResource = function () {
         $state.go('searchCar', $stateParams)
     }
 
-    $scope.spot = resourceService[$stateParams.type].filter(s => s.id == $stateParams.id)[0];
+    $scope.addAvailability = function () {
+        alert('Select availability range to add');
+    }
+
+    $scope.removeAvailability = function () {
+        alert('Select availability range to remove');
+    }
 
 });
-    
