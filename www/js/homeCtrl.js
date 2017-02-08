0be24e6601f81bed40d3d91e9b274b1314a4fe90
Nigh7Sh4de <plotnikd@gmail.com>
2017-02-08 14:23:19 +0100

    Add markers to map based on spots

--- www/js/homeCtrl.js
+++ www/js/homeCtrl.js
@@ -1,5 +1,13 @@
 angular.module('starter').controller("Home", function($scope, $stateParams, $state, $cordovaGeolocation) {
 
+    var spots = [{
+        location: { coordinates: [43.65, -79.38]}
+    }, {
+        location: { coordinates: [43.655, -79.385]}
+    }, {
+        location: { coordinates: [43.66, -79.39]}
+    }]
+
     var mapOptions = {
         center: new google.maps.LatLng(43.656140, -79.381085),
         zoom: 14,
@@ -9,13 +17,25 @@ angular.module('starter').controller("Home", function($scope, $stateParams, $sta
     };
     $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
+    var icon = {
+        url: 'img/icon Final-Logo copy.png',
+        scaledSize: new google.maps.Size(40, 40)
+    }
+
+    google.maps.event.addListenerOnce($scope.map, 'idle', function() {
+        spots.forEach(function(spot) {
+            var marker = new google.maps.Marker({
+                map: $scope.map,
+                animation: google.maps.Animation.DROP,
+                icon: icon,
+                position: new google.maps.LatLng(spot.location.coordinates[0], spot.location.coordinates[1])
+            })
+        })
+    })
+
     var options = {timeout: 10000, enableHighAccuracy: false};
-
     $cordovaGeolocation.getCurrentPosition(options).then(function(position) {
-        var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-        $scope.map.setCenter(latLng);
-        
-
+        $scope.map.setCenter(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
     }, function(error){
         console.log("Could not get location");
     });
