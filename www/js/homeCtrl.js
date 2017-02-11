23e5e9f8b0b78bb4adac98d1e4ebd1b23e398650
Dennis Plotnik <Dennis Plotnik>
2017-02-11 23:29:18 +0100

    Add search bar

--- www/js/homeCtrl.js
+++ www/js/homeCtrl.js
@@ -13,9 +13,37 @@ angular.module('starter').controller("Home", function($scope, $stateParams, $sta
         zoom: 14,
         maximumAge: 1000*60*60*24*365,
         fullscreenControl: false,
+        mapTypeControl: false,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+
+    $scope.changePlace = function() {
+        console.log('location', $scope.location)
+    }
+
+
+    var input = document.getElementById('mapSearch');
+    var searchBox = new google.maps.places.SearchBox(input);
+    $scope.map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
+        if (places.length == 0)
+            return;
+
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function(place) {
+            if (place.geometry.viewport)
+                bounds.union(place.geometry.viewport);
+            else
+                bounds.extend(place.geometry.location);
+        });
+        $scope.map.fitBounds(bounds);
+    });
+
+
 
     var icon = {
         url: 'img/icon Final-Logo copy.png',
@@ -23,6 +51,7 @@ angular.module('starter').controller("Home", function($scope, $stateParams, $sta
     }
 
     google.maps.event.addListenerOnce($scope.map, 'idle', function() {
+        $(".pac-container").attr('data-tap-disabled','true');
         spots.forEach(function(spot) {
             var marker = new google.maps.Marker({
                 map: $scope.map,
