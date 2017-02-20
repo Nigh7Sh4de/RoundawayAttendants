3cf2aa24bdb12433aa5bf84eec59f52a6f45ae7e
Anthony Layne <anthony.layne@td.com>
2017-02-20 14:39:55 -0500

    deleted unnecessary files

--- www/js/profileBarCtrl.js
+++
@@ -1,8 +0,0 @@
-angular.module('starter').controller("ProfileBarWidgetController", function ($scope, $stateParams, $state, userInfoService) {
-    userInfoService.getProfileInfo().then(function(userInfo) {
-        $scope.name = userInfo.name;
-    }).catch(function(err) {
-        console.error('Error ' + err + 'retrieving userInfo')
-        $scope.showAlert(err.message)
-    });
-});
